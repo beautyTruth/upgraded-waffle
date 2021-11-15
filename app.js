63d44d0ea2be284f48f54d41a669cea1c19a95c7
Phil <happy.melodist@gmail.com>
2021-11-15 16:09:00 -0600

    complete dynamic coding for dark mode toggle

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const checkBox = document.querySelector('input[name="theme"]');
+// const checkBox = document.querySelector('input[name="theme"]');
+// let htmlElement = document.documentElement;
+
+// checkBox.addEventListener("click", function () {
+//   if (checkBox.checked) {
+//     htmlElement.classList.toggle("transition");
+//     htmlElement.setAttribute("data-theme", "dark");
+//   } else {
+//     htmlElement.classList.toggle("transition");
+//     htmlElement.setAttribute("data-theme", "light");
+//   }
+// });
+
+const checkBox = document.querySelector("input[name='theme']");
+/* when you're selecting the HTML element and an attribute, the attribute goes into brackets after the element -- keep in mind this is a big question you had before */
 let htmlElement = document.documentElement;
 
-checkBox.addEventListener("click", function () {
+// console.log(htmlElement);
+// console.log(checkBox);
+
+checkBox.addEventListener("click", () => {
   if (checkBox.checked) {
     htmlElement.classList.toggle("transition");
     htmlElement.setAttribute("data-theme", "dark");
